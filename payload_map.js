6aa415cef07c006d7913b6e47589ccbc2d5565b1
Mugiwara <37186888+ciss84@users.noreply.github.com>
2025-03-08 18:32:18 +0100

    Add files via upload

---
+++ payload_map.js
@@ -0,0 +1,14 @@
+// @ts-check
+const CUSTOM_ACTION_APPCACHE_REMOVE = "appcache-remove";
+const payload_map = [
+	  {
+        displayTitle: "kstuff",
+        description: "FPKG enabler",
+        fileName: "kstuff.elf",
+        author: "kstuff",
+        projectSource: "",
+        binarySource: "",
+        version: "5.10",
+		    toPort: 9021
+    }
+];
